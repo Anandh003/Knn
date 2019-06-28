8fc76754285abeec5c2feefb47017b9a372204bb
Anandh <anandh@localhost.localdomain>
2019-06-28 07:25:35 +0530

    Create find_distance function. ==> Find distance between data points and centroids.
    Find the minimum norm.
    
    Signed-off-by: Anandh <anandh@localhost.localdomain>

--- knn.py
+++ knn.py
@@ -1,58 +1,32 @@
 import os
 import math
+import random
 import numpy as np
 import pandas as pd
 import scipy.io
 from matplotlib import pyplot as plt
 
 
-# def plot_data(x, y, kind="plot",
-#               align="center",
-#               x_axis="X_axis",
-#               y_axis="Y_axis",
-#               title="Graph",
-#               color="r",
-#               marker="x",
-#               size=20,
-#               label="y"):
-#     plt.ion()
-#     if kind == "scatter":
-#         plt.scatter(x, y,
-#                     color=color,
-#                     label=label,
-#                     s=size,
-#                     marker=marker)
-#     elif kind == "plot":
-#         plt.plot(x, y,
-#                  color=color,
-#                  marker=marker,
-#                  label=label,
-#                  markersize=size)
-#     else:
-#         raise ValueError("Kind: {} is not valid".format(kind))
-#     plt.xlabel(x_axis)
-#     plt.ylabel(y_axis)
-#     plt.title(title)
-# plt.show()
 class Knn():
     def __init__(self, data, groups=2):
         self.clusters = groups
         self.feature = data
         self.rows = data.shape[0]
         self.columns = data.shape[1]
-        self.centroids = data.sample(n=groups)
-        self.centroid_idx = pd.DataFrame(np.zeros((self.rows, 1), dtype=int))
+        self.centroids = random.sample(list(self.feature), groups)
+        self.centroid_idx = np.zeros((self.rows, 1), dtype=int)
 
     def fit(self):
-        distances = np.zeros(self.rows, self.groups)
+        norms = self.find_distance()
+        self.centroid_idx = np.argmin(norms, axis=1)
+
+    def find_distance(self):
+        temp = np.zeros((self.rows, self.clusters))
         for i in range(self.clusters):
-            for j in range(self.rows):
-                distances[j, i] = math.sqrt(centroids[j:])
-
-
-# def create_centroids():
-
+            temp[:, i] = np.sqrt(np.sum(np.square(self.feature - i), axis=1))
+        return temp
 
 X = scipy.io.loadmat(os.path.join(os.getcwd(), "src", "ex7data2.mat"))
-X = pd.DataFrame(X['X'])
+X = X['X']
 obj = Knn(X, 3)
+obj.fit()
